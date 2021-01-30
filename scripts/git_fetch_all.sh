2a3b1c85c57b39330653b628d15fc55152cce45c
Max <max@keyless.io>
2021-01-30 18:18:46 +0100

    using ssh-agent

--- scripts/git_fetch_all.sh
+++ scripts/git_fetch_all.sh
@@ -2,10 +2,15 @@
 
 if [[ $# -eq 0 ]]; then
     echo "ERROR: Provide a dir"
+    echo "./git_fetch_all.sh /dir /path_to_key"
     exit 1
 fi
 
 MY_DIR=$1
+PATH_TO_SSH_KEY=$2
+
+eval `ssh-agent`
+ssh-add $PATH_TO_SSH_KEY
 
 find $MY_DIR -name .git -type d -prune | while read d; do
     cd $d/..

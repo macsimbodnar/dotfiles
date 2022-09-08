72cad9fcf0e33f4611d5852e281ca419370a302f
Maksym Bodnar <macsimbodnar@gmail.com>
2022-09-08 14:58:29 +0200

    Add tar alias

--- fish/config.fish
+++ fish/config.fish
@@ -8,28 +8,18 @@ set -g EDITOR vim
 # Public
 alias pip="pip3"
 alias python="python3"
-alias format="astyle --suffix=none --options=$HOME/.astylerc --recursive '*.cpp,*.h,*.hpp' | grep '^Formatted'"
-alias check-format="astyle --suffix=none --options=$HOME/.astylerc --recursive --dry-run '*.cpp,*.h,*.hpp' | grep '^Formatted'"
-
-alias local-check-format="astyle --suffix=none --options=./.circleci/.astylerc --recursive --dry-run '*.cpp,*.h,*.hpp' | grep '^Formatted'"
-alias local-format="astyle --suffix=none --options=./.circleci/.astylerc --recursive '*.cpp,*.h,*.hpp' | grep '^Formatted'"
+alias list-source-files="find . -iname '*.h' -o -iname '*.c' -o -iname '*.hpp' -o -iname '*.cpp'"
+alias format="list-source-files | xargs clang-format -i"
+alias check-format="list-source-files | xargs clang-format --dry-run -Werror"
 
 alias fcc="check-format"
 alias fpp="format"
-alias lfcc="local-check-format"
-alias lfpp="local-format"
 
 alias valg="valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose --log-file=valgrind-report.txt"
 
 alias set-ass="export ASAN_OPTIONS=detect_leaks=0:halt_on_error=0:strict_string_checks=1:detect_stack_use_after_return=1:check_initialization_order=1:strict_init_order=1; export ASAN_ENABLED=1"
 
 alias unset-ass="set -e ASAN_OPTIONS ; set -e ASAN_ENABLED"
-
-alias scan="clamscan -r --bell -i"
-
-alias penv="source venv/bin/activate"
-
-alias makes="make clean ; make all -j(getconf _NPROCESSORS_ONLN) ; make test -j(getconf _NPROCESSORS_ONLN)"
 
 alias vasm="~/ws/vasm/vasm6502_oldstyle"
 
@@ -41,6 +31,9 @@ alias lines-of-code="cloc ."
 
 alias build="rm -rf build && mkdir build && cd build"
 alias rebuild="cd .. && rm -rf build && mkdir build && cd build"
+
+alias do-tar="tar -czf"
+alias undo-tar="tar -xzf"
 
 # Sourece non public shit
 if test -e ~/.config/fish/private_config.fish
